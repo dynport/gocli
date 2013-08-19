5e5c20cad032921ceb653fbf82c4016d4f5ff86c
Tobias Schwab <tobias.schwab@dynport.de>
2013-08-19 23:45:40 +0200

    add example binary for testing

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
 default: test
 
-test:
+build:
+	go build -o ./bin/gocli
+
+test: build
 	go test -v
+	./bin/gocli
+	./bin/gocli co
+	./bin/gocli co sta
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("gocli test script")
+	router := NewRouter(
+		map[string]*Action {
+			"container/start": {
+				Description: "Start container",
+				Usage: "<container_id>",
+				Handler: func(args *Args) error {
+					fmt.Println("ACTION: start container")
+					return nil
+				},
+			},
+			"container/stop": {
+				Description: "Stop container",
+				Usage: "<container_id>",
+				Handler: func(args *Args) error {
+					fmt.Println("ACTION: stop container", args.Args)
+					return nil
+				},
+			},
+			"image/list": {
+				Description: "List Images",
+				Handler: func(args *Args) error {
+					fmt.Println("ACITON: list images")
+					return nil
+				},
+			},
+		},
+	)
+	router.Separator = " "
+	router.Handle(os.Args)
+}
